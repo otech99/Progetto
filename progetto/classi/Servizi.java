2dfaa31fbecbcb040856ba5ac0e1b5b2add8bb01
aleandro1219 <aleandromartini02@gmail.com>
2023-04-02 17:56:13 +0200

    g

--- progetto/classi/Servizi.java
+++ progetto/classi/Servizi.java
@@ -3,7 +3,7 @@ package progetto.classi;
 public class Servizi {
     private String nome; //nome del un servizio
     private int Nporte; //numero di porte
-    private int Nvul; //numero di vulnerabilità
+    private int Nvul; //numero di vulnerabilitàll
 
     public Servizi(String nome, int Nporte, int Nvul){
         this.nome= nome;
