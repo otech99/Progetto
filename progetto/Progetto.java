e575183d4aceaa63d7ad1953684c975e75607424
aleandro1219 <aleandromartini02@gmail.com>
2023-04-02 18:22:07 +0200

    g

--- progetto/Progetto.java
+++ progetto/Progetto.java
@@ -1,16 +1,23 @@
 package progetto;
 
+import progetto.classi.Servizi;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Progetto {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
 
+        ArrayList<Servizi> servizi = new ArrayList();
+
         int numServizi = scan.nextInt();
         int numZone = scan.nextInt();
         int numServer = scan.nextInt();
 
-        System.out.println(numServer + " " + numServizi + " " + numZone);
+        for(int i = 0; i < numServizi; i++){
+
+        }
 
     }
 }
