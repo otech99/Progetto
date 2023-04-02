a5e7d1d8e703de8d8c77c972f0dbeb4da5753bfe
aleandro1219 <aleandromartini02@gmail.com>
2023-04-02 18:14:58 +0200

    g

--- progetto/Progetto.java
+++ progetto/Progetto.java
@@ -1,9 +1,16 @@
 package progetto;
 
+import java.util.Scanner;
+
 public class Progetto {
     public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("ciao3");
+        int numServizi = scan.nextInt();
+        int numZone = scan.nextInt();
+        int numServer = scan.nextInt();
+
+        System.out.println(numServer + " " + numServizi + " " + numZone);
 
     }
 }
