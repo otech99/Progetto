93fadcd8d4d582f7ab17c93e6db131ff45de8067
aleandro1219 <129616645+aleandro1219@users.noreply.github.com>
2023-04-11 17:22:12 +0200

    Update Task3.java

--- progetto/TASK/Task3.java
+++ progetto/TASK/Task3.java
@@ -1,4 +1,39 @@
 package progetto.TASK;
 
+import progetto.classi.Server;
+
+import java.util.Scanner;
+
 public class Task3 {
+
+    public static void input(Server[] server){
+        Scanner scan = new Scanner(System.in);
+        int numAttacchi = scan.nextInt();
+        String[] IdServerAtt = new String[numAttacchi];
+        for(int i=0; i<numAttacchi; i++){
+            IdServerAtt[i] = scan.next();
+        }
+        for(Server s: server){
+            for(int i=0; i<numAttacchi; i++){
+                if(s.getId().equals(IdServerAtt[i])){
+                    s.setNumAttacchi(s.getNumAttacchi()+1); //
+                    int serverListDim = s.getServerList().size(); //Preleva la dimensione della Server List
+                    for (int j=0; j<serverListDim; j++) {
+                        for (Server e : server) { //Re-itero di nuovo per ogni server
+                            if(e.getId().equals(s.getServerList().get(j).getId())) { // e se l' id del server è uguale all' id corrente della server list
+                                e.setNumAttacchi(e.getNumAttacchi()+1);
+                            }
+
+                        }
+                    }
+
+                    }
+                }
+
+
+            }
+
+        }
+
+    }
 }
